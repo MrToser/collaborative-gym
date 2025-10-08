5ec34cd0f5b480758a21b78e1bb619eef4506b3b
MrToser <zxsjh888@163.com>
2025-10-08 14:01:34 +0800

    reproduce paper

---
+++ scripts/try.sh
@@ -0,0 +1,6 @@
+python -m scripts.fully_auto_agent_exp \
+  --task "tabular_analysis" \
+  --start-idx 0 \
+  --end-idx 100 \
+  --team-member-config-path configs/teams/auto_agent_team_config_deepseek.toml \
+  --result-dir-tag results/deepseek
